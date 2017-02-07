db95862ea0b5f2cebdae63d11a11334e59841016
John Adams <jna@retina.net>
2017-02-07 02:34:14 -0800

    Final rev2, clean up rev1

---
+++ fabrication-rev2/macrofab-rename.sh
@@ -0,0 +1,7 @@
+mv badge-kicad-Edge.Cuts.gm1 badge-kicad-Edge.Cuts.bor
+#mv badge-kicad-In1.Cu.g2 badge-kicad-In1.Cu.g2l
+#mv badge-kicad-In2.Cu.g3 badge-kicad-In2.Cu.g3l
+#mv badge-routed-In1.Cu.g2 badge-routed-In1.Cu.g2l
+#mv badge-routed-In2.Cu.g3 badge-routed-In2.Cu.g3l
+
+
